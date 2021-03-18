30f98365aa8d3b5f9370813b806d01133c98ce1c
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-03-18 14:06:40 +0900

    Initial Commit

---
+++ Keyboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Keyboard
+//
+//  Created by 澤崎正佳 on 2021/03/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Keyboard/KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardApp.swift
+//  Keyboard
+//
+//  Created by 澤崎正佳 on 2021/03/18.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
